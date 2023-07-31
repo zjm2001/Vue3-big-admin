8a22c3728b32f764b36b06fcca5fcbbb18b0033a
zjm <2215539769@qq.com>
2023-07-31 14:57:21 +0800

    完成elementPlus的自动按需导入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 <template>
   <h1>我是app入口组件</h1>
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
 </template>
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+// createRouter 创建路由实例，===> new VueRouter()
+// 1. history模式: createWebHistory()   http://xxx/user
+// 2. hash模式: createWebHashHistory()  http://xxx/#/user
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), //
   routes: []
 })
 
